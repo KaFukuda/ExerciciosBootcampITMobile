fe712eecc145c2f1bf8229d94b98224d2ec6d21b
Karina Fukuda <karina.fukuda@mercadolivre.com>
2022-06-18 17:54:43 -0300

    added file For.kt

---
+++ src/main/kotlin/For.kt
@@ -0,0 +1,33 @@
+fun main(){
+    //contagem(50)
+    //contagemRegressiva(50)
+    //contagemSemMultiplos5(30)
+    //somaTotal(500)
+}
+
+fun contagem ( num: Int ){
+    for (num in 1..num step 1)
+        print("$num - ")
+}
+
+fun contagemRegressiva ( i: Int ){
+    for ( i in i downTo 1 step 1){
+        print("$i - ")
+    }
+}
+
+fun contagemSemMultiplos5 ( j : Int ){
+    for ( j in 1..j step 1) {
+        if (j %5 != 0){
+            print("$j - ")
+        }
+    }
+}
+
+fun somaTotal ( k: Int ){
+    var soma = 0
+    for ( k in soma..k){
+        soma += k
+    }
+    println(soma)
+}
